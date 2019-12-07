f1d0cfee3197c436fff06a3fe91d5d907beab410
Ádám István Weber <adam.weber@stud.uni-corvinus.hu>
2019-12-07 17:19:19 +0100

    firstcommit

--- UserMaintenance_1/Form1.cs
+++ UserMaintenance_1/Form1.cs
@@ -15,6 +15,9 @@ namespace UserMaintenance_1
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName;
+            label2.Text = Resource1.FirstName;
+            button1.Text = Resource1.Add;
         }
     }
 }
